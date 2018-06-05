3608cd907b1208a330cd833aeb90b250be8c173f
darbio <james@darb.io>
2018-06-05 18:31:26 +1000

    webpack removed

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
-const path = require('path');
-const slsw = require('serverless-webpack');
+const path = require("path");
+const slsw = require("serverless-webpack");
+var nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-    entry: slsw.lib.entries,
-    mode: 'development',
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js', '.json', '.ts', '.tsx']
-    },
-    externals: ['aws-sdk'],
-    output: {
-        libraryTarget: 'commonjs',
-        path: path.join(__dirname, '.webpack'),
-        filename: '[name].js'
-    },
-    target: 'node',
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.mjs$/,
-                include: /node_modules/,
-                type: 'javascript/auto'
-            }
+  entry: slsw.lib.entries,
+  mode: "development",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".json", ".ts", ".tsx"]
+  },
+  // externals: ['aws-sdk'],
+  externals: [nodeExternals()],
+  output: {
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js"
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
         ]
-    }
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      }
+    ]
+  }
 };
